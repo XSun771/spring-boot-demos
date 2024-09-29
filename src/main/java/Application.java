eb2d8a8bac83016c3c99699b01732e746e558a6b
Xavier <771401651@qq.com>
2024-09-29 13:29:12 +0800

    grabled-chars-1.0

---
+++ src/main/java/Application.java
@@ -0,0 +1,8 @@
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Application {
+    public static void main(String[] args) throws InterruptedException {
+        log.info("Simplified Chinese 简体中文");
+    }
+}
--- study-https/src/main/resources/start-server.sh
+++
@@ -1,2 +0,0 @@
-cd /root/target/
-nohup java -jar -Dspring.profiles.active=server ./study-https-0.0.1-SNAPSHOT.jar > study-https.log 2>&1 &
